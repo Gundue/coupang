bd18c8efe3a8f7fc3d32f8ed41bf9bfcb49eff2d
gunwoo <gunwoo1919@naver.com>
2020-12-22 12:31:10 +0900

    Initial commit

---
+++ src/test/java/com/coupang/demo/CoupangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coupang.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

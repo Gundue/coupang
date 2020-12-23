c93f505923765139f805c194eae98c09d0e23206
gunwoo <gunwoo1919@naver.com>
2020-12-23 21:45:32 +0900

    feat : Route파일들을 추가한다

--- src/main/java/com/coupang/route/ProductRoute.java
+++ src/main/java/com/coupang/route/ProductRoute.java
@@ -1,5 +1,9 @@
 package com.coupang.route;
 
+import com.coupang.datamodel.dto.ProductDTO;
+import com.coupang.repository.ProductRepository;
+import com.coupang.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/product")
 public class ProductRoute {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductRoute(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping("/{productId}")
-    public void getProduct(@PathVariable (value = "productId") String productId) {}
+    public ProductDTO getProduct(@PathVariable (value = "productId") String productId) throws Exception{
+        return this.productService.productById(productId);
+    }
+
+    @GetMapping("/initializeProducts")
+    public void initializeProducts() {
+        this.productService.initializeProducts();
+    }
 
     @GetMapping("/gender")
     public void recommandGender() {}
--- src/main/java/com/coupang/route/UserRoute.java
+++ src/main/java/com/coupang/route/UserRoute.java
@@ -1,26 +1,77 @@
 package com.coupang.route;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.coupang.datamodel.dto.BucketDTO;
+import com.coupang.datamodel.dto.ReviewDTO;
+import com.coupang.datamodel.dto.SaleDTO;
+import com.coupang.datamodel.dto.UserDTO;
+import com.coupang.model.Bucket;
+import com.coupang.service.BucketService;
+import com.coupang.service.ReviewService;
+import com.coupang.service.SaleService;
+import com.coupang.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserRoute {
+    private final UserService userService;
+    private final SaleService saleService;
+    private final BucketService bucketService;
+    private final ReviewService reviewService;
+
+    @Autowired
+    public UserRoute(UserService userService, SaleService saleService, BucketService bucketService, ReviewService reviewService) {
+        this.userService = userService;
+        this.saleService = saleService;
+        this.bucketService = bucketService;
+        this.reviewService = reviewService;
+    }
 
     @GetMapping("/{userId}")
-    public void getUser(@PathVariable(value = "userId") String userId) {}
+    @ResponseBody
+    public UserDTO getUser(@PathVariable(value = "userId") String userId) throws Exception{
+        return this.userService.userById(userId);
+    }
+    @GetMapping("/initialize")
+    public void initialize() {
+        this.userService.initialize();
+    }
 
     @GetMapping("/{userId}/salelist")
-    public void getSalelist(@PathVariable(value = "userId") String userId) {}
+    public SaleDTO getSalelist(@PathVariable(value = "userId") String userId) throws Exception{
+        return this.saleService.userById(userId);
+    }
+
+    @GetMapping("/initializeSales")
+    public void initializeSales() {
+        this.saleService.initializeSales();
+    }
 
     @GetMapping("/{userId}/totalprice")
-    public void getTotalprice(@PathVariable(value = "userId") String userId) {}
+    public SaleDTO getTotalprice(@PathVariable(value = "userId") String userId) throws Exception{
+        return this.saleService.getTotalAmountByUser(Integer.parseInt(userId));
+    }
 
     @GetMapping("/{userId}/bucketlist")
-    public void getBucket(@PathVariable (value = "userId") String userId) {}
+    public BucketDTO getBucket(@PathVariable (value = "userId") String userId) throws Exception{
+        return this.bucketService.userById(userId);
+    }
+
+    @GetMapping("/initializeBucket")
+    public void initializeBucket() {
+        this.bucketService.initializeBucket();
+    }
 
     @GetMapping("/{userId}/review")
-    public void getReview(@PathVariable (value = "userId") String userId) {}
+    public ReviewDTO getReview(@PathVariable (value = "userId") String userId) throws Exception{
+        return this.reviewService.userById(userId);
+    }
+
+    @GetMapping("/initializeReview")
+    public void initializeReview() {
+        this.reviewService.initializeReview();
+    }
+
 }

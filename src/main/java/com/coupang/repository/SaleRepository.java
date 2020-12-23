3c33f1bd532e2dde999eb82c20fa16f5fd782b6e
gunwoo <gunwoo1919@naver.com>
2020-12-23 21:44:56 +0900

    feat : List 추가

--- src/main/java/com/coupang/repository/SaleRepository.java
+++ src/main/java/com/coupang/repository/SaleRepository.java
@@ -4,6 +4,9 @@ import com.coupang.model.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, String> {
+    public List<Sale> findByUserId(String userId);
 }
